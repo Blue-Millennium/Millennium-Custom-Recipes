e8006fd79a8787be54cff1d823b87f24e64c5bbb
TPOD14077 <qwertyuiop14077@qq.com>
2024-05-11 16:20:15 +0800

    添加了鞘翅的配方

---
+++ src/main/java/org/millennium/suisurorus/MCR/Custom_Recipes/Elytra.java
@@ -0,0 +1,46 @@
+package org.millennium.suisurorus.MCR.Custom_Recipes;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class Elytra implements Listener {
+
+    public void initCustomRecipes() {
+        Material[] bannerColors = {
+                Material.WHITE_BANNER,
+                Material.ORANGE_BANNER,
+                Material.MAGENTA_BANNER,
+                Material.LIGHT_BLUE_BANNER,
+                Material.YELLOW_BANNER,
+                Material.LIME_BANNER,
+                Material.PINK_BANNER,
+                Material.GRAY_BANNER,
+                Material.LIGHT_GRAY_BANNER,
+                Material.CYAN_BANNER,
+                Material.PURPLE_BANNER,
+                Material.BLUE_BANNER,
+                Material.BROWN_BANNER,
+                Material.GREEN_BANNER,
+                Material.RED_BANNER,
+                Material.BLACK_BANNER
+        };
+
+        for (Material colorBanner : bannerColors) {
+            NamespacedKey key = new NamespacedKey("mcr", colorBanner.name().toLowerCase() + "_to_elytra");
+            ShapedRecipe ely = new ShapedRecipe(key, new ItemStack(Material.ELYTRA))
+                    .shape("ABA", "CDC", "EFE")
+                    .setIngredient('A', Material.SLIME_BALL)
+                    .setIngredient('B', Material.CHORUS_FLOWER)
+                    .setIngredient('C', Material.PHANTOM_MEMBRANE)
+                    .setIngredient('D', colorBanner)
+                    .setIngredient('E', Material.FEATHER)
+                    .setIngredient('F', Material.DRAGON_BREATH);
+            getServer().addRecipe(ely);
+        }
+    }
+}
--- src/main/java/org/millennium/suisurorus/MCR/Millennium_Custom_Recipes.java
+++ src/main/java/org/millennium/suisurorus/MCR/Millennium_Custom_Recipes.java
@@ -1,17 +1,24 @@
 package org.millennium.suisurorus.MCR;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.millennium.suisurorus.MCR.Custom_Recipes.Elytra;
 
 public final class Millennium_Custom_Recipes extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Elytra(), this);
+        new Elytra().initCustomRecipes();
+        System.out.println("MCR has been loaded.");
         // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
+        Bukkit.resetRecipes();
+        System.out.println("MCR has been uninstalled.");
         // Plugin shutdown logic
     }
 }
