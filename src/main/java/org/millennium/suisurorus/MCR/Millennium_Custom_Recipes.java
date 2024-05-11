faa9e237f9f441a833b1c437966eb94daf871aaa
TPOD14077 <qwertyuiop14077@qq.com>
2024-05-11 15:35:45 +0800

    初始化仓库

---
+++ src/main/java/org/millennium/suisurorus/MCR/Millennium_Custom_Recipes.java
@@ -0,0 +1,17 @@
+package org.millennium.suisurorus.MCR;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Millennium_Custom_Recipes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
